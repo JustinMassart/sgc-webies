9e8cf47bc76401a107a7ccbebaf8efee437bc737
JustinMassart <72126592+JustinMassart@users.noreply.github.com>
2020-11-13 13:21:16 +0100

    Updated index file (switched to PHP)

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>VERSION HTML - Cinema A Entertainment Category Flat Bootstarp Resposive Website Template | Home :: w3layouts</title>
+<title>Webies - Cinéma fictif</title>
 <link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
 <!-- Custom Theme files -->
 <link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
