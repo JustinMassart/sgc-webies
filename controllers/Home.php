870ccf61da796582ff356316cc536cef56bb39d8
JustinMassart <72126592+JustinMassart@users.noreply.github.com>
2020-12-11 13:13:30 +0100

    Added things

--- controllers/Home.php
+++ controllers/Home.php
@@ -4,11 +4,13 @@ class Home
 {
     public $header;
     public $links;
+    public $featured;
 
     public function __construct($header)
     {
         $this->header = $header;
         $this->links = NavigationLink::getAllDisplayed();
+        $this->featured = Movie::getFeatured();
     }
 
     public function render()
